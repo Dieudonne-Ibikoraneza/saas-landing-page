42211972565c6617ddef42eb3d7778a5f7006c80
Dieudonne-Ibikoraneza <dieudonneibikoraneza13@gmail.com>
2024-10-11 15:22:43 +0200

    Header section - halfway there

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import Header from "./sections/Header";
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="overflow-hidden">
+      <Header />
+    </main>
+  );
 };
 
 export default App;
